aa8dae3523daa9329ffd9adb197ccaad92687bb3
shawonetc <157738136+shawonetc@users.noreply.github.com>
2025-03-07 16:06:33 +0600

    Update User.js

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   googleId: { type: String, required: true }, // For Google OAuth
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  picture: { type: String }, // Optional: Profile Picture URL
+  picture: String,  // Store the Google profile picture URL
 });
 
 module.exports = mongoose.model("User", userSchema);
